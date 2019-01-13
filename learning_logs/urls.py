cb67a42f538022659028afaaaa50d17dca3bc56a
MrHonker <982143642@qq.com>
2019-01-13 22:43:12 +0800

    完成功能： 添加topic,添加entry，编辑entry 完成至19.1.3

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,6 +12,10 @@ urlpatterns = [
 	path('topics/', views.topics, name='topics'),
 	# path('topics/1/', views.topics, name='topics'),
 	re_path('topics/(?P<topic_id>\d+)/', views.topic, name='topic'),
+	re_path('new_topic/', views.new_topic, name='new_topic'),
+	re_path('new_entry/(?P<topic_id>\d+)/', views.new_entry, name='new_entry'),
+	url('edit_entry/(?P<entry_id>\d+)/', views.edit_entry,
+		name='edit_entry'),
 	#re_path('topics/(?P<topic_id>\d+)/', views.topic, name='topic'),
 ]
 
