6568d0926c0747cb34e5b8cd7658d037177734bb
MrHonker <982143642@qq.com>
2019-02-17 16:25:32 +0800

    19.2.4 完成，实现注册功能

--- users/urls.py
+++ users/urls.py
@@ -3,11 +3,13 @@ from django.contrib.auth.views import login
 from django.urls import path
 from . import views
 
+# admin Changeme_123  ;用户名和密码
+
 app_name = 'users'
 urlpatterns = [
     # 登录页面
     path('login/', login, {'template_name': 'users/login.html'}, name='login'),
     path('logout/', views.logout_view, name='logout'),
-
+    path('register/', views.register, name='register'),
 
 ]
