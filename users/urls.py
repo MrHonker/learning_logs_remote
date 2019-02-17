c95e272ac8b91166f41c3610a88165de49d4ad34
MrHonker <982143642@qq.com>
2019-02-17 16:02:46 +0800

    19.2.3 已完成 完成功能：login 和logout 下一步：完成注册账户

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,11 @@ from django.contrib.auth.views import login
 from django.urls import path
 from . import views
 
+app_name = 'users'
 urlpatterns = [
     # 登录页面
-    path('login/$', login, {'template_name': 'users/login.html'}, name='login'),
+    path('login/', login, {'template_name': 'users/login.html'}, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 
 
 ]
